742516be286feef8260e47ef70c17c0210c7a9e4
Ivan Sidarau <isidarau@360t.com>
2015-10-14 16:53:49 +0200

    deleted public abstract void updateStock(Stock stock); method from StockStorage (to create feeling like it read-only interface

--- src/main/java/stsc/common/storage/StockStorage.java
+++ src/main/java/stsc/common/storage/StockStorage.java
@@ -5,11 +5,13 @@ import java.util.Set;
 
 import stsc.common.stocks.Stock;
 
+/**
+ * {@link StockStorage} interface. <br/>
+ * Common way to operate with {@link Stock}'s storage.
+ */
 public interface StockStorage {
 
-	public abstract Optional<Stock> getStock(String name);
-
-	public abstract void updateStock(Stock stock);
+	public abstract Optional<Stock> getStock(final String name);
 
 	public abstract Set<String> getStockNames();
 }
